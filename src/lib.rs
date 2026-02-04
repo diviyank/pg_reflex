60b9c333b25d439fd889ba90a567d18fc0f3293a
Diviyan Kalainathan <diviyan@fentech.ai>
2026-02-04 16:31:12 +0100

    [DEV] Added pre-init

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,26 @@ use pgrx::prelude::*;
 
 ::pgrx::pg_module_magic!(name, version);
 
+/// This SQL will be executed exactly once when 'CREATE EXTENSION' is run.
+/// Collate "C" for faster lookups
+extension_sql!(
+    r#"
+    CREATE TABLE IF NOT EXISTS public.__reflex_ivm_reference (
+        name TEXT PRIMARY KEY COLLATE "C",
+        graph_level INT NOT NULL,
+        depends_on TEXT[],
+        depends_on_imv TEXT[],
+        sql_query TEXT,
+        parsed_sql_query JSON,
+        indexes TEXT[],
+    );
+    
+    -- You can also add indexes here
+    CREATE INDEX IF NOT EXISTS idx__reflex_ivm_name ON public.__reflex_ivm_reference(name);
+    "#,
+    name = "pg_reflex_init", // Unique name for this SQL block
+);
+
 #[pg_extern]
 fn hello_pg_reflex() -> &'static str {
     "Hello, pg_reflex"
@@ -16,7 +36,6 @@ mod tests {
     fn test_hello_pg_reflex() {
         assert_eq!("Hello, pg_reflex", crate::hello_pg_reflex());
     }
-
 }
 
 /// This module is required by `cargo pgrx test` invocations.
